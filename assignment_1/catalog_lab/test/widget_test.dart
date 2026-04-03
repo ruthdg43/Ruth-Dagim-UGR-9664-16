51f59de509882473829f8153dc21b41cbd1f515e
ruthdg43 <ruthafruta97@gmail.com>
2026-04-03 21:47:01 +0300

    hhhh

--- assignment_1/catalog_lab/test/widget_test.dart
+++ assignment_1/catalog_lab/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:catalog_lab/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const CatalogApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
